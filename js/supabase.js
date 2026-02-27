0800f7aa8a8209f9f2e26302d22ebf004d4df968
harjbains <46567842+harjbains@users.noreply.github.com>
2026-02-27 20:20:03 +0000

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://vfgjurflmmdyvjlzxibo.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_bLS0D6uno93rC5C0Bi9FNw_7dr4paCt";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
