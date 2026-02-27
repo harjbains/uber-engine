0b36e865d5b2813e115d56832e91c7d96d298e2c
harjbains <46567842+harjbains@users.noreply.github.com>
2026-02-27 20:30:39 +0000

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,9 @@
 const SUPABASE_URL = "https://doaokmhdfwdwtkwxxksx.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_r_oYKrDrzU-BGgNIerSu9w_DSLnSTLJ"; // replace with full key
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_r_oYKrDrzU-BGgNIerSu9w_DSLnSTLJ";
 
 const { createClient } = window.supabase;
 
 const supabaseClient = createClient(
   SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  SUPABASE_PUBLISHABLE_KEY
 );
