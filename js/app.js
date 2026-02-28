2e5f819f490aea490d5dd672a727f819312c3071
harjbains <46567842+harjbains@users.noreply.github.com>
2026-02-28 06:27:30 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ document.getElementById("current-date").innerText =
   now.toLocaleDateString() + " " + now.toLocaleTimeString();
 
 const APP_VERSION = "v0.1.1";
-const APP_CHANGELOG = "5-minute rounding + numeric keyboard fix 2";
+const APP_CHANGELOG = "5-minute rounding + numeric keyboard fix 3";
 
 document.getElementById("build-version").innerText =
   APP_VERSION + " – " + APP_CHANGELOG;
@@ -101,12 +101,6 @@ function roundToFiveMinutes(timeString) {
   );
 }
 
-document.getElementById("start-time").addEventListener("input", function () {
-  this.value = roundToFiveMinutes(this.value);
-});
 
-document.getElementById("end-time").addEventListener("input", function () {
-  this.value = roundToFiveMinutes(this.value);
-});
 
 console.log("Rounded:", roundToFiveMinutes("09:17"));
