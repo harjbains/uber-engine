974ce47bd339823e823d34261fada25ee9c2aaf5
Harry Baines <harjbains@gmail.com>
2026-03-04 19:33:52 +0000

    v0.6.7 - Fix delete buttons + mobile layout

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ import { initFuel } from "./fuel.js";
 import { initMonthly } from "./monthly.js";
 import { initVersion } from "./version.js";
 
+const APP_VERSION = "v0.6.6";
+
+document.getElementById("app-version").textContent = APP_VERSION;
+
 document.addEventListener("DOMContentLoaded", () => {
   initTabs();
   initShifts();
